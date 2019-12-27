8dfe76aaf81ef9a5e4f83750b6465fdb71cf88d3
salvadora <salvadora@zam-id-saac>
2019-12-27 16:21:42 -0600

    Curso spring Seccion 3 video 26

--- src/main/java/mx/com/lobos/controllers/ItemController.java
+++ src/main/java/mx/com/lobos/controllers/ItemController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import mx.com.lobos.models.Item;
+import mx.com.lobos.models.Producto;
 import mx.com.lobos.models.service.ItemService;
 
 @RestController
@@ -25,8 +28,21 @@ public class ItemController {
 		return itemService.finAll();
 	}
 	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")	
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.finById(id, cantidad);
+	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(545.45);
+		item.setProducto(producto);
+		return item;
 	}
 }
